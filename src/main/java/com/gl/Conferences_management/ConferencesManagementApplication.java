97532667d2897a45efeb4e637b785c8917cc960b
rambaburonte <ronterambabu@gmail.com>
2025-11-19 15:44:17 +0530

    feat: Add CORS configuration to allow cross-origin requests

--- src/main/java/com/gl/Conferences_management/ConferencesManagementApplication.java
+++ src/main/java/com/gl/Conferences_management/ConferencesManagementApplication.java
@@ -2,6 +2,9 @@ package com.gl.Conferences_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ConferencesManagementApplication {
@@ -10,4 +13,17 @@ public class ConferencesManagementApplication {
 		SpringApplication.run(ConferencesManagementApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(false);
+			}
+		};
+	}
 }
